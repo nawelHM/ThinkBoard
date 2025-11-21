4dc8519ccdf770ddafedbee38ad37035eabbb1f6
Nawel Hammouda <nawelks.info@gmail.com>
2025-11-21 12:48:09 +0100

    axios update

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,19 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./src/middelware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname =path.resolve()
 //middleware
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
   origin: "http://localhost:5174"
 }));
+}
 app.use(express.json()); // pour lire req.body , this middelware will parse Json bodies
 app.use(rateLimiter);
 
@@ -24,6 +27,14 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/notes", noteRoutes);
+
+
+ if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+ }
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("server started on Port: ", PORT);
